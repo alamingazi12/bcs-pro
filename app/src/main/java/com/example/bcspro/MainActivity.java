431e8629656a6a6d07e2c4994cc795668f87146c
PHP <alamingazi68@gmail.com>
2020-06-04 23:06:17 -0700

    Bcs Pro

--- app/src/main/java/com/example/bcspro/MainActivity.java
+++ app/src/main/java/com/example/bcspro/MainActivity.java
@@ -1,32 +1,71 @@
 package com.example.bcspro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setFontToActionBar();
+        setNavigation();
+        final NavController navController= Navigation.findNavController(this,R.id.my_nav_host_fragment);
+        // NavigationUI.setupActionBarWithNavController(this,Navigation.findNavController(this,R.id.my_nav_host_fragment));
+        BottomNavigationView navigationView=findViewById(R.id.bottom_nav);
+        //NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                switch (item.getItemId()){
+
+
+                    case R.id.home:
+                        navController.navigate(R.id.homefragment);
+                        break;
+                    case R.id.search:
+                        navController.navigate(R.id.profilefragment);
+                        break;
+                    case R.id.account:
+                        navController.navigate(R.id.morefragment);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void setNavigation() {
+    }
+
+    private void setFontToActionBar() {
         TextView tv = new TextView(getApplicationContext());
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
         tv.setLayoutParams(lp);
         tv.setText("BCS Pro");
-                tv.setTextSize(20);
+        tv.setTextSize(20);
         tv.setTextColor(Color.parseColor("#FFFFFF"));
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/PlayfairDisplay-Black.otf");
         tv.setTypeface(tf);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(tv);
-        Log.d("method","onCreate");
+        setFontToActionBar();
     }
 
     @Override
---
+++ app/src/main/java/com/example/bcspro/fragment/FragmentProfile.java
@@ -0,0 +1,40 @@
+package com.example.bcspro.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.bcspro.R;
+
+public class FragmentProfile extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.navigation_profile_fragment,container,false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(getActivity(), R.id.my_nav_host_fragment);
+
+        //Button button = view.findViewById(R.id.button_frag1);
+
+    }
+}
